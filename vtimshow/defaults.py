1b9cbe638da5f4bdc5fb490c0f20fac81a5999bf
kprussing <kprussing74@gmail.com>
2015-05-04 16:35:00 -0400

    Add more default values

--- vtimshow/defaults.py
+++ vtimshow/defaults.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""
+Default parameters for the module.
+
+"""
+
+import os
 
 AUTHOR = "Keith F Prussing"
 AUTHOR_EMAIL = "kprussing74@gmail.com"
@@ -7,4 +13,6 @@ PLUGIN_CLASS = "VtImageViewer"
 PLUGIN_NAME = "Image Viewer"
 COMMENT = "Display data sets as images"
 VERSION = "{VERSION!s}"
+UID = "image_viewer"
+FOLDER, MODULE_NAME = os.path.split(os.path.dirname(__file__))
 
