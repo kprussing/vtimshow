9c6cb0943381271052e11d35df7c35c677c52cdf
kprussing <kprussing74@gmail.com>
2015-05-04 16:39:48 -0400

    Add initial corrections to class
    Now the class loads and does not crash.  I added the help information;
    however, it does not do anything yet.

--- vtimshow/vtimageviewer.py
+++ vtimshow/vtimageviewer.py
@@ -1,7 +1,50 @@
 #!/usr/bin/env python3
 
-class VtImageViewer:
+from . import defaults
+from . import __version__
 
-    def __init__(self):
-        print("Woot!")
+from PyQt4 import QtGui
+
+class VtImageViewer:
+    """
+    """
+    UID = defaults.UID
+    NAME = defaults.PLUGIN_NAME
+    COMMENT = defaults.COMMENT
+
+    def __init__(self, parent=None):
+        #super(VtImageViewer, self).__init__(parent)
+
+
+    def helpAbout(self, parent):
+        """Full description of the plugin.
+
+        This is adapted from the code used in the ``ImportCSV`` class
+        distributed with ViTables.
+        """
+        from vitables.plugins.aboutpage import AboutPage
+        desc = {
+            "version" : __version__,
+            "module_name" : defaults.MODULE_NAME,
+            "folder" : defaults.FOLDER,
+            "author" : "{0:s} <{1:s}>".format(
+                defaults.AUTHOR, defaults.AUTHOR_EMAIL
+            ),
+            "comment" : QtGui.QApplication.translate(
+                defaults.PLUGIN_CLASS,
+                """
+                <qt>
+                <p>View 2D data set as an image.</p>
+                <p>
+                If the data set is simply 2D, view it as an image.  If
+                the dataset is 3D of dimension (N,M,K), view each (N,M)
+                slice [0,K) as an image with a slider.
+                </p>
+                </qt>
+                """,
+                "Text of an About plugin message box"
+            )
+        }
+        about_page = AboutPage(desc, parent)
+        return about_page
 
