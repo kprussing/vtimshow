efde1f51d0202f738aa0bfc76c3840b3d8cfc648
kprussing <kprussing74@gmail.com>
2015-05-05 10:26:41 -0400

    Make defaults a dictionary
    Now all of the default parameters are a hidden dictionary in the top of
    the package.

--- vtimshow/__init__.py
+++ vtimshow/__init__.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python3
-import logging
-logging.getLogger().addHandler(logging.NullHandler())
 
-from . import defaults
+# Module imports
+import logging
+import os
+
+import vitables
+
+_defaults = dict(
+    AUTHOR = "Keith F Prussing",
+    AUTHOR_EMAIL = "kprussing74@gmail.com",
+    LICENSE = "MIT",
+    PLUGIN_CLASS = "VtImageViewer",
+    PLUGIN_NAME = "Image Viewer",
+    COMMENT = "Display data sets as images",
+    VERSION = "{VERSION!s}",
+    UID = "image_viewer"
+)
+_defaults["FOLDER"], _defaults["MODULE_NAME"] = os.path.split(
+    os.path.dirname(__file__)
+)
+_defaults["LOGGER"] = logging.getLogger(_defaults["MODULE_NAME"])
+_defaults["LOGGER"].addHandler(logging.NullHandler())
 
 __docformat__ = "restructuredtext"
-__version__ = defaults.VERSION
+__version__ = _defaults["VERSION"]
 
-plugin_class = defaults.PLUGIN_CLASS
-plugin_name = defaults.PLUGIN_NAME
-comment = defaults.COMMENT
+plugin_class = _defaults["PLUGIN_CLASS"]
+plugin_name = _defaults["PLUGIN_NAME"]
+comment = _defaults["COMMENT"]
 
-from .vtimageviewer import VtImageViewer
+from vtimshow.vtimageviewer import VtImageViewer
 
