47be6e5cfa5898d8b94408e5645c60ca7287c5d8
kprussing <kprussing74@gmail.com>
2015-07-30 11:05:19 -0400

    Initial completion of multi cube math
    It pretty much works.  I'm still double checking to see if it is
    actually working the way I think it is.

--- vtimshow/multicubemath.py
+++ vtimshow/multicubemath.py
@@ -14,6 +14,7 @@ from vitables.vtapp import translate as _translate
 
 from . import _defaults
 from .colorrow import ColorRow
+from .utils import divide
 
 class MultiCubeMath(QtGui.QMdiSubWindow):
     """The class to perform cross data set frame math.
@@ -26,7 +27,7 @@ class MultiCubeMath(QtGui.QMdiSubWindow):
 
     def __init__(self, parent):
         """Initialize the cube math window.
-        
+
         Parameters
         ----------
 
@@ -47,20 +48,6 @@ class MultiCubeMath(QtGui.QMdiSubWindow):
             ))
 
 
-        self.red_index = indexes[0]
-        if len(indexes) > 1:
-            self.green_index = indexes[1]
-        else:
-            self.green_index = None
-
-        if len(indexes) > 2:
-            self.blue_index = indexes[2]
-        else:
-            self.blue_index = None
-
-        self._update_dbt_leaf()
-        self.pindex = None
-
         widget = QtGui.QWidget(parent)
         self.setWidget(widget)
 
@@ -71,22 +58,10 @@ class MultiCubeMath(QtGui.QMdiSubWindow):
         self.image_view = pyqtgraph.ImageView(parent=self.widget())
         self._layout.addWidget(self.image_view, 0, 0, 1, 1)
         self._layout.setRowStretch(0, 10)
+        self._layout.setColumnStretch(0, 10)
 
         self._add_color_panels(indexes)
-        #self._red = ColorRow(self, index=self.red_index, color="r")
-        #self._red.setTitle("R")
-        #self._layout.addWidget(self._red, 1, 0, 1, 1)
-
-        #self._green = ColorRow(self, index=self.green_index, color="g")
-        #self._green.setTitle("G")
-        #self._layout.addWidget(self._green, 2, 0, 1, 1)
-
-        #self._blue = ColorRow(self, index=self.blue_index, color="b")
-        #self._blue.setTitle("B")
-        #self._layout.addWidget(self._blue, 3, 0, 1, 1)
-
-        #for it in range(1,4):
-            #self._layout.setRowStretch(it, 1)
+        self._add_math_group()
 
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
         self.setWindowTitle(_translate(
@@ -94,21 +69,8 @@ class MultiCubeMath(QtGui.QMdiSubWindow):
         ))
         self.setVisible(True)
 
-    def _update_dbt_leaf(self):
-        """Have the ``dbt_leaf`` mirror one of the leaves.
-        
-        This ensures that at least one node is assigned to the leaf.  If
-        it were not, we would get obnoxious error when the application
-        was closed with subwindow open.  It cycles through three colors
-        looking for the first one that is not ``None`` and uses that
-        leaf.  If all values are ``None``, it does not change the leaf.
-
-        """
-        databases = vitables.utils.getGui().dbs_tree_model
-        for color in self._colors.values():
-            if color.index os not None:
-                self.dbt_leaf = databases.nodeFromIndex(index)
-                break
+        self._update_dbt_leaf()
+        self.pindex = None
 
     def _add_color_panels(self, indexes):
         """Add the color channels to the window.
@@ -142,7 +104,9 @@ class MultiCubeMath(QtGui.QMdiSubWindow):
             self._colors[color].setTitle(color)
             self._layout.addWidget(self._colors[color], row, 0, 1, 1)
             self._layout.setRowStretch(row, 1)
-
+            self._colors[color].frame_changed.connect(
+                self._update_image
+            )
 
     def _add_math_group(self):
         """Create a panel to hold the math options.
@@ -154,6 +118,7 @@ class MultiCubeMath(QtGui.QMdiSubWindow):
         self._math_layout.setSpacing(0)
 
         buttons = (
+            ("R", self._show_r),
             ("RGB", self._show_rgb),
             ("R - G", self._show_r_minus_g),
             ("R / G", self._show_r_by_g),
@@ -168,8 +133,118 @@ class MultiCubeMath(QtGui.QMdiSubWindow):
             button.clicked.connect(function)
             self._math_layout.addWidget(button, row, 0, 1, 1)
 
-        self._layout.addWidget(
+        self._layout.addWidget(self._math_group, 1, 1, 3, 1)
+        self._update_math_group()
 
-        )
+    def _get_frames(self):
+        """Programmatically get the frames."""
+        R = self._colors["Red"].get_frame()
+        G = self._colors["Green"].get_frame()
+        B = self._colors["Blue"].get_frame()
+        return R, G, B
 
+    def _show_r(self):
+        """Show the R band image in monochrome."""
+        R, G, B = self._get_frames()
+        #self.image_view.getImageItem().updateImage(R)
+        # Calling ``updateImage`` also works, but the brightness range
+        # is not automatically updated.  So, just set the image for now.
+        self.image_view.setImage(R)
+
+    def _show_rgb(self):
+        """Show the RGB image."""
+        R, G, B = self._get_frames()
+        image = numpy.dstack((R, G, B))
+        self.image_view.setImage(image)
+
+    def _show_r_minus_g(self):
+        """Show :math:`R - G`."""
+        R, G, B = self._get_frames()
+        self.image_view.setImage(R - G)
+
+    def _show_r_by_g(self):
+        """Show :math:`R / G`."""
+        R, G, B = self._get_frames()
+        self.image_view.setImage(divide(R, G))
+
+    def _show_r_minus_g_by_b(self):
+        """Show :math:`(R - G) / B`."""
+        R, G, B = self._get_frames()
+        self.image_view.setImage(divide(R - G, B))
+
+    def _update_dbt_leaf(self):
+        """Have the ``dbt_leaf`` mirror one of the leaves.
+
+        This ensures that at least one node is assigned to the leaf.  If
+        it were not, we would get obnoxious error when the application
+        was closed with subwindow open.  It cycles through three colors
+        looking for the first one that is not ``None`` and uses that
+        leaf.  If all values are ``None``, it does not change the leaf.
+
+        """
+        databases = vitables.utils.getGui().dbs_tree_model
+        for color in self._colors.values():
+            if color.index is not None:
+                self.dbt_leaf = databases.nodeFromIndex(color.index)
+                break
+
+    def _update_image(self):
+        """Call all routines after updating the image."""
+        self._update_math_group()
+        button = self._math_buttons.checkedButton()
+        if button is not None and button.isEnabled():
+            button.click()
+
+
+    def _update_math_group(self):
+        """Ensure only reasonable math can be performed.
+
+        When a leaf node has been changed, we need to make sure only
+        rational math can be performed.  I a particular color has been
+        set to the null value, we disable any math that uses that color.
+        After that check, we take the red channel as the standard and
+        make sure that we can generate a sane image.  For example, it
+        makes no sense to use 4D images with RGB.  Also, we must be able
+        to broadcast the images together to perform the math.
+
+        """
+        for button in self._math_buttons.buttons():
+            button.setEnabled(True)
+
+        if self._colors["Red"].index is None:
+            for button in self._math_buttons.buttons():
+                button.setEnabled(False)
+
+            return
+
+        for key in ("Green", "Blue"):
+            if self._colors[key].index is None:
+                for button in self._math_buttons.buttons():
+                    if key[0] in button.text():
+                        button.setEnabled(False)
+
+        rgb = False
+        compatible = True
+        dims = self._colors["Red"].data.shape
+        for key, val in self._colors.items():
+            if key == "Red" or val.index is None:
+                continue
+
+            rgb |= (val.data.ndim == 4) | (
+                (val.data.ndim == 3) & val.node_is_2d()
+            )
+            for d1, d2 in zip(dims[::-1], val.data.shape[::-1]):
+                compatible &= (d1 == 1) | (d2 == 1) | (d1 == d2)
+
+        for button in self._math_buttons.buttons():
+            if not button.isEnabled() or button.text() == "R":
+                # Skip any button that has already been flagged as
+                # invalid, and let the 'R' button stay active because we
+                # know the red channel is populated.
+                continue
+
+            if button.text() == "RGB":
+                button.setEnabled(not rgb)
+            else:
+                button.setEnabled(compatible)
 
